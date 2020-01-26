08dbba70e3b74dc7743e4112644604af5f2a8ad1
Jose Rojas <jmrojasc@uni-bremen.de>
2020-01-26 02:17:01 +0100

    Adding semantic Camera

--- src/control-avatar.lisp
+++ src/control-avatar.lisp
@@ -6,6 +6,7 @@
 (defvar *csl-command-srv* nil "name of ROS service for sending console commands")
 (defvar *command-srv* nil "name of ROS service for sending commands")
 (defvar *move-to-srv* nil "name of ROS service for moving to a location")
+(defvar *semantic-cam-srv* nil "name of ROS service for calling Semantic Camera")
 
 (defun init-ros-avatar (name)
   "Binds services. `name specifies the name of the turtle."
@@ -13,6 +14,7 @@
   (setf *csl-command-srv* (concatenate 'string "/" name "/send_console_command"))
   (setf *command-srv* (concatenate 'string "/" name "/send_command"))
   (setf *move-to-srv* (concatenate 'string "/" name "/move_to"))
+  (setf *semantic-cam-srv* (concatenate 'string "/" name "/semantic_camera"))
   (setf *pose-sub* (subscribe (format nil "~a/Pose" name)"geometry_msgs/Pose"
                               #'pose-cb))
   (setf *cmd-vel-pub* (advertise (format nil "/twistmsg")
@@ -37,5 +39,9 @@
   (call-service *move-to-srv* 'iai_avatar_msgs-srv:MoveTo
                 :position (roslisp:make-message
                             'geometry_msgs-msg:point :x x :y y :z z)))
+
+(defun call-semantic-camera-service ()
+  "Function to call the semantic camera service."
+  (call-service *semantic-cam-srv* 'iai_avatar_msgs-srv:SemanticCamera))
 
 
