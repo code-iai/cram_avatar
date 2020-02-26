28e54164d1d450a212c2d0609afeefec6de6a1dd
Mona Abdel-Keream <abdelker@uni-bremen.de>
2020-02-26 17:29:54 +0100

    Redefined action-designators

--- src/action-designators.lisp
+++ src/action-designators.lisp
@@ -2,22 +2,22 @@
  
 (def-fact-group avatar-action-designators (action-grounding)
 
-  (<- (desig:action-grounding ?desig (add-topping ?from ?on))
+  (<- (desig:action-grounding ?desig (add-topping ?desig))
     (desig-prop ?desig (:type  :adding-topping))
     (desig-prop ?desig (:from  ?from))
     (desig-prop ?desig (:on    ?on)))
 
-  (<- (desig:action-grounding ?desig (pick-up ?object ?with-hand))
+  (<- (desig:action-grounding ?desig (pick-up ?desig))
     (desig-prop ?desig (:type      :picking-up))
     (desig-prop ?desig (:object    ?object))
     (desig-prop ?desig (:with-hand ?with-hand)))
 
-  (<- (desig:action-grounding ?desig (put-down ?from-hand ?location))
+  (<- (desig:action-grounding ?desig (put-down ?desig))
     (desig-prop ?desig (:type      :putting-down))
     (desig-prop ?desig (:from-hand ?from-hand))
     (desig-prop ?desig (:at        ?location)))
 
-  (<- (desig:action-grounding ?desig (pour-over ?object))
+  (<- (desig:action-grounding ?desig (pour-over ?desig))
     (desig-prop ?desig (:type   :pouring-over))
     (desig-prop ?desig (:object ?object)))
 )
--- src/high-level-plans.lisp
+++ src/high-level-plans.lisp
@@ -1,6 +1,6 @@
 (in-package :aia)
 
-(defvar *container* "SM_Milk_Jug_5" "Name for the topping container")
+(defvar *container* "SM_Milk_Jug_0" "Name for the topping container")
 (defvar *obj_target* "SM_Waffle_2" "Name for target object")
 (defvar *hand* "right" "Hand to be used")
 
@@ -16,7 +16,7 @@
         (exe:perform (desig:an action (type adding-topping) (from ?container) (on ?target))))))
   )
 ;; Interpreting adding topping action
-(defun add-topping (?from ?on)
+(defun add-topping (&key ((:from ?from)) ((:on ?on)) &allow-other-keys)
 
   (let ((?loc_vector *container_loc* ) (?hand *hand*))
     (exe:perform (desig:an action (type picking-up) (object ?from) (with-hand ?hand)))
@@ -26,40 +26,43 @@
     (exe:perform (desig:an action (type putting-down) (from-hand ?hand) (at ?loc_vector)))))
 
 ;; Interpreting picking up action
-(defun pick-up (?object ?with-hand)
+(defun pick-up (&key ((:object ?object)) ((:with-hand ?with-hand)) &allow-other-keys)
     
-    (let ((trgt (desig:a motion (type looking) (target ?object))))
-      (pm-execute 'avatar-navigation trgt))
+  ;;  (let ((trgt (desig:a motion (type looking) (target ?object))))
+  ;;    (pm-execute 'avatar-navigation trgt))
     (let ((target (desig:a motion (type grasping) (item ?object) (hold t))))
       (pm-execute 'avatar-manipulation target))
     (sleep 3)
-    (let ((trgt (desig:a motion (type looking) (target "front"))))
-      (pm-execute 'avatar-navigation trgt)))
+  ;;  (let ((trgt (desig:a motion (type looking) (target "front"))))
+  ;;   (pm-execute 'avatar-navigation trgt))
+  )
 
 ;; Interpreting putting down action
-(defun put-down (?from-hand ?location)
+(defun put-down (&key ((:from-hand ?from-hand)) ((:at ?location)) &allow-other-keys)
   (let ((?in_x (float (cl-transforms:x ?location) 1.0)) 
         (?in_y (float (cl-transforms:y ?location) 1.0)) 
         (?in_z (float (cl-transforms:z ?location) 1.0 )))
 
-    (let ((target (desig:a motion (type looking) (x_val ?in_x) (y_val ?in_y) (z_val ?in_z))))
-      (pm-execute 'avatar-navigation target))
+   ;; (let ((target (desig:a motion (type looking) (x_val ?in_x) (y_val ?in_y) (z_val ?in_z))))
+   ;;   (pm-execute 'avatar-navigation target))
     (let ((target (desig:a motion (type placing) (from_hand ?from-hand) (x_val ?in_x) (y_val ?in_y) (z_val ?in_z))))
       (pm-execute 'avatar-manipulation target))
     (sleep 3)
-    (let ((trgt (desig:a motion (type looking) (target "front"))))
-      (pm-execute 'avatar-navigation trgt))))
+   ;; (let ((trgt (desig:a motion (type looking) (target "front"))))
+    ;;   (pm-execute 'avatar-navigation trgt))
+    ))
 
 ;; Interpreting pouring action
-(defun pour-over (?object)
+(defun pour-over (&key ((:object ?object)) &allow-other-keys) 
 
-    (let ((trgt (desig:a motion (type looking) (target ?object))))
-      (pm-execute 'avatar-navigation trgt))
+   ;; (let ((trgt (desig:a motion (type looking) (target ?object))))
+   ;;   (pm-execute 'avatar-navigation trgt))
     (let ((trgt (desig:a motion (type pouring) (target ?object))))
       (pm-execute 'avatar-manipulation trgt))
     (sleep 9)
-    (let ((trgt (desig:a motion (type looking) (target "front"))))
-        (pm-execute 'avatar-navigation trgt)))
+   ;; (let ((trgt (desig:a motion (type looking) (target "front"))))
+  ;;     (pm-execute 'avatar-navigation trgt))
+  )
 
 
 
