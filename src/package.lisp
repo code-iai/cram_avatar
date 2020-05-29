59b6d4edd8fd86b8c04cf9cfbaaa6d18e8605cf0
Michael Neumann <neumann.michael@uni-bremen.de>
2020-05-29 10:01:45 +0200

    add export qna service call

--- src/package.lisp
+++ src/package.lisp
@@ -2,4 +2,8 @@
   (:nicknames :aia)
   (:use :cpl :roslisp :cl-transforms :cram-designators :cram-process-modules
         :cram-language-designator-support)
-  (:import-from :cram-prolog :def-fact-group :<- :lisp-fun))
+  (:import-from :cram-prolog :def-fact-group :<- :lisp-fun)
+  (:export
+   :call-qna-action
+   )
+  )
