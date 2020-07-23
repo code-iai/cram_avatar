7e39cae1d7bc74896b42d2bb2ae29ac102b5f421
Jose Rojas <jmrojasc@uni-bremen.de>
2020-07-23 20:23:49 +0200

    uncommenting some calls

--- src/high-level-plans.lisp
+++ src/high-level-plans.lisp
@@ -82,12 +82,12 @@ ing) (target ?target)))
       (sleep 2)
       (pm-execute 'avatar-navigation (desig:a motion (type looking) (target ?person)))
       (sleep 4)
-      ;;(exe:perform (desig:an action (type reading-book) (book ?book)))
+      (exe:perform (desig:an action (type reading-book) (book ?book)))
       (let ((cmd (concatenate 'string "slide right " (desig:desig-prop-value ?book :name))))
           (call-send-console-command cmd))
       (sleep 6)
       ;; Wait for step 2
-      ;;(wait-for (eq (fl-funcall #'std_msgs-msg:data *sync-state*) 2))
+      (wait-for (eq (fl-funcall #'std_msgs-msg:data *sync-state*) 2))
 
       ;; Feed caretaker
       ;;(exe:perform (desig:an action (type moving-on-path) (path ?place_3)))
