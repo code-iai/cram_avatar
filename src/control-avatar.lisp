9adcba2017b876f332e81a2676f1b444597a5969
Mona Abdel-Keream <abdelker@uni-bremen.de>
2022-02-16 09:59:46 +0100

    added fluent for belief state

--- src/control-avatar.lisp
+++ src/control-avatar.lisp
@@ -1,5 +1,9 @@
 (in-package :aia)
 
+(defvar *belief-state* (make-fluent :name :belief-state :value 0) "current belief state of avatar")
+(defvar *belief-sub* nil "belief ROS subscriber")
+;;(defvar *belief-pub* nil "belief ROS publisher")
+
 (defvar *sync-state* (make-fluent :name :sync-state :value 0) "current sync state between robot and avatar")
 (defvar *sync-sub* nil "sync ROS subscriber")
 (defvar *sync-pub* nil "sync ROS publisher")
@@ -19,20 +23,31 @@
   (setf *command-srv* (concatenate 'string "/" name "/send_command"))
   (setf *move-to-srv* (concatenate 'string "/" name "/move_to"))
   (setf *semantic-cam-srv* (concatenate 'string "/" name "/semantic_camera"))
-
   (setf *pose-sub* (subscribe (format nil "~a/Pose" name)"geometry_msgs/Pose"
                               #'pose-cb))
-
   (init-sub)
-
-  (setf *sync-pub* (advertise (format nil "avatar_robot/Sync")"std_msgs/Int32")))
+  (setf *sync-pub* (advertise (format nil "avatar_robot/Sync")"std_msgs/Int32"))
+  (setf *belief-sub* (subscribe (format nil "~a/Belief" name)"std_msgs/Int32"
+                              #'belief-cb))
+)
 
 (defun init-sub ()
   (setf *sync-sub* (subscribe (format nil "avatar_robot/Sync")"std_msgs/Int32"
                               #'sync-cb)))
+
+
+defun wait-for-belief-state (state)
+      (wait-for (eq (fl-funcall #'std_msgs-msg:data *belief-state*) state)))
+
+;;(defun get-belief-state (state)
+  ;;(publish *belief-pub* (make-message "std_msgs/Int32" :data state)))
+
+(defun belief-cb (msg)
+  (setf (value *belief-state*) msg))
+
+                              
 (defun wait-for-sync-state (state)
-      (wait-for (eq (fl-funcall #'std_msgs-msg:data *sync-state*) state))
-  )
+      (wait-for (eq (fl-funcall #'std_msgs-msg:data *sync-state*) state)))
 
 (defun send-sync-state (state)
   "Function to send sync state"
@@ -41,6 +56,7 @@
 (defun sync-cb (msg)
   "Callback for sync values. Called by the sync topic subscriber."
   (setf (value *sync-state*) msg))
+
 
 (defun pose-cb (msg)
   "Callback for pose values. Called by the pose topic subscriber."
